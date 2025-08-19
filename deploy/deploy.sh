35b07308d31e8cc271d5c5e9aad8e8cf662a9331
catmeow123456 <eden@stu.pku.edu.cn>
2025-08-19 16:55:58 +0800

    small fix to deploy bash

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,8 +13,5 @@ echo "设置正确的权限..."
 sudo chown -R www-data:www-data /var/www/ai4s-blog
 sudo chmod -R 755 /var/www/ai4s-blog
 
-echo "重新加载nginx配置..."
-sudo systemctl reload nginx
-
 echo "部署完成！网站已更新。"
-echo "访问地址: http://10.129.242.36"
+echo "访问地址: http://aislab.pku.edu.cn"
