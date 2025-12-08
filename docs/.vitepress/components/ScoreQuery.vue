946d28ae63db88380d899a8087489597a7c81a97
catmeow123456 <eden@stu.pku.edu.cn>
2025-12-08 13:35:31 +0800

    更新到第五次小作业

--- docs/.vitepress/components/ScoreQuery.vue
+++ docs/.vitepress/components/ScoreQuery.vue
@@ -113,7 +113,7 @@ const results = ref([])
 const hasSearched = ref(false)
 
 const servers = ['58', '132', '197']
-const MAX_ASSIGNMENTS = 4
+const MAX_ASSIGNMENTS = 5
 
 async function fetchCSV(url) {
   try {
